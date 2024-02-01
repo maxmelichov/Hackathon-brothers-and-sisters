4160341cdd5069025c1ccb969ea2713bf6701107
Maxim Melichov <80150303+maxmelichov@users.noreply.github.com>
2024-02-01 16:36:56 +0200

    Update nlp_model.py

--- nlp_model.py
+++ nlp_model.py
@@ -1,33 +1,25 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from openai import OpenAI
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
 import torch
-import re
-import openai
-import config
-from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
 from sklearn.model_selection import train_test_split
 from datasets import Dataset, DatasetDict
+import openai
+import config
 
-
-openai.api_key=config.key
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
-MODEL_NAME = "meta-llama/Llama-2-7b-chat-hf"
+openai.api_key = config.key
 
-def load_data(dir_file:str) -> pd.DataFrame:
+def load_data(dir_file: str) -> pd.DataFrame:
     df = pd.read_csv(dir_file)
     return df
 
-def comine_dataframes(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+def combine_dataframes(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     df = pd.concat([df1, df2], ignore_index=True)
     return df
 
 def get_tokenizer(df: pd.DataFrame, model_name: str) -> pd.DataFrame:
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    # Tokenize all texts at once
     outputs = tokenizer(df["text"].tolist(), padding=True, truncation=True)
-    # Add tokenized outputs to dataframe
     df["input_ids"] = outputs["input_ids"]
     df["attention_mask"] = outputs["attention_mask"]
     return df, tokenizer
@@ -40,24 +32,17 @@ def load_model(model_name: str) -> torch.nn.Module:
     return model
 
 def split_data(df: pd.DataFrame) -> DatasetDict:
-    
-    # Assuming df is your DataFrame
-
-    # Split the DataFrame into train and temp (which will be split again)
     df_train, df_temp = train_test_split(df, test_size=0.3)
-    # Split the temp DataFrame into validation and test
     df_val, df_test = train_test_split(df_temp, test_size=0.5)
 
     df_train.reset_index(drop=True, inplace=True)
     df_val.reset_index(drop=True, inplace=True)
     df_test.reset_index(drop=True, inplace=True)
 
-    # Convert the DataFrames to datasets
     train_dataset = Dataset.from_pandas(df_train)
     val_dataset = Dataset.from_pandas(df_val)
     test_dataset = Dataset.from_pandas(df_test)
 
-    # Combine the datasets into a DatasetDict
     dataset_dict = DatasetDict({
         'train': train_dataset,
         'validation': val_dataset,
@@ -65,7 +50,7 @@ def split_data(df: pd.DataFrame) -> DatasetDict:
     })
     return dataset_dict
 
-def get_model_args():
+def get_model_args(dataset_dict: DatasetDict):
     batch_size = 8
     logging_steps = len(dataset_dict["train"]) // batch_size
     model_name = f"distilbert-base-cased-finetuned"
@@ -98,18 +83,16 @@ if "__main__" == __name__:
     Suicide_Detection["class"] = Suicide_Detection["class"].replace("suicide",1)
     Suicide_Detection["class"] = Suicide_Detection["class"].replace("non-suicide",0)
     Suicide_Detection.rename(columns={"class": "label"}, inplace=True)
-    print(1)
     depression_dataset_reddit_cleaned = load_data(r"data\depression_dataset_reddit_cleaned.csv")
     depression_dataset_reddit_cleaned.rename(columns={"clean_text": "text", "is_depression": "label"}, inplace=True)
-    print(2)
-    df = comine_dataframes(Suicide_Detection,depression_dataset_reddit_cleaned)
+
+    df = combine_dataframes(Suicide_Detection,depression_dataset_reddit_cleaned)
     df.reset_index(inplace=True, drop=True)
-    print(3)
+
     df, tokenizer = get_tokenizer(df, "distilbert-base-cased")
-    print(4)
+
     model = load_model("distilbert-base-cased")
     dataset_dict = split_data(df)
-    print(5)
-    training_args = get_model_args()
+
+    training_args = get_model_args(dataset_dict)
     trainer = train_model(model, dataset_dict, training_args, tokenizer)
-    print(2)
